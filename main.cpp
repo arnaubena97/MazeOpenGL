b33e4b5dc50b5aae82baf8ee32d1c0863d5981b1
MarcFelip <marc.felip.pomes@gmail.com>
2021-10-08 11:51:45 +0200

    Es pot cambiar el observador

--- main.cpp
+++ main.cpp
@@ -22,12 +22,17 @@
 //-----------------------------------------------
 //              GLOBAL VARIABLES
 //-----------------------------------------------
+/*--- Global variables that determine the viewpoint location ---*/
+int anglealpha = 90;
+int anglebeta = 60;
+#define PI 3.1416
+
 
 #define MED_COLUMNS 6
 #define MED_ROWS 6
 #define SIZE_SQUARE_SMALL 3 // quant mes petit
-#define WIDTH 700
-#define HEIGHT 700
+#define WIDTH 900
+#define HEIGHT 900
 
 int COLUMNS = MED_COLUMNS * 2 + 1;
 int ROWS = MED_ROWS * 2 + 1;
@@ -48,6 +53,7 @@ Square agent2;
 //-----------------------------------------------
 //                FUNCTIONS
 //-----------------------------------------------
+void PositionObserver(float alpha,float beta,int radi);
 void randomMove();
 void display();
 void chargeSquares();
@@ -63,23 +69,26 @@ Square square;
 
 int main(int argc,char *argv[])
 {
+    anglealpha=90;
+    anglebeta=30;
 
     chargeSquares();
 
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowPosition(700, 400);
     glutInitWindowSize(WIDTH, HEIGHT);
     glutCreateWindow("Maze board");
-        
+    glEnable(GL_DEPTH_TEST);
+
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
     glutSpecialFunc(ArrowKey);
 
     glutIdleFunc(idle);
 
-    glMatrixMode(GL_PROJECTION);
-    gluOrtho2D(0,WIDTH-1,0,HEIGHT-1);
+    //glMatrixMode(GL_PROJECTION);
+    //gluOrtho2D(0,WIDTH-1,0,HEIGHT-1);
 
     glutMainLoop();
     return 0;
@@ -118,7 +127,22 @@ void chargeSquares(){
 
 void display() {
     glClearColor(1.0,1.0,1.0,1.0);
-    glClear(GL_COLOR_BUFFER_BIT); 
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    PositionObserver(anglealpha,anglebeta,1000);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-WIDTH*0.2,WIDTH,-HEIGHT*0.2,HEIGHT,10,2000);
+
+    glMatrixMode(GL_MODELVIEW);
+
+    glPolygonMode(GL_FRONT,GL_FILL);
+    glPolygonMode(GL_BACK,GL_LINE);
+
     wall.draw(); // walls
     start.draw(); //start point
     endsa.draw(); // end point
@@ -165,11 +189,19 @@ void ArrowKey(int key,int x,int y){
 };
 
 void keyboard(unsigned char key, int x, int y){
-    switch (key) {
-        case 27: // exit with ESC
+ 
+    if (key ==27)
         exit(0);
-        break;
-    }
+    else if (key=='i' && anglebeta<=(90-4))
+        anglebeta=(anglebeta+3);
+    else if (key=='k' && anglebeta>=(-90+4))
+        anglebeta=anglebeta-3;
+    else if (key=='j')
+        anglealpha=(anglealpha+3)%360;
+    else if (key=='l')
+        anglealpha=(anglealpha-3+360)%360;
+
+
     glutPostRedisplay();
 };
 
@@ -234,4 +266,42 @@ void randomMove(){
             }  
         }
     
+}
+
+void PositionObserver(float alpha,float beta,int radi){
+  float x,y,z;
+  float upx,upy,upz;
+  float modul;
+
+  x = (float)radi*cos(alpha*2*PI/360.0)*cos(beta*2*PI/360.0);
+  y = (float)radi*sin(beta*2*PI/360.0);
+  z = (float)radi*sin(alpha*2*PI/360.0)*cos(beta*2*PI/360.0);
+
+  if (beta>0)
+    {
+      upx=-x;
+      upz=-z;
+      upy=(x*x+z*z)/y;
+    }
+  else if(beta==0)
+    {
+      upx=0;
+      upy=1;
+      upz=0;
+    }
+  else
+    {
+      upx=x;
+      upz=z;
+      upy=-(x*x+z*z)/y;
+    }
+
+
+  modul=sqrt(upx*upx+upy*upy+upz*upz);
+
+  upx=upx/modul;
+  upy=upy/modul;
+  upz=upz/modul;
+
+  gluLookAt(x,y,z,    0.0, 0.0, 0.0,     upx,upy,upz);
 }
